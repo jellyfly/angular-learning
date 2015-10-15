447296c59544fc8a65b41dd8f20ce5f9edd098b9
jelly_fly <jelly329@126.com>
2015-10-14 22:37:18 -0400

    data binding practices

---
+++ 5/app.js
@@ -0,0 +1,10 @@
+angular.module('myApp', [])
+.controller('MyController', function($scope, $timeout) {
+	var updateClock = function() {
+		$scope.clock = new Date();
+		$timeout(function() {
+			updateClock();
+		}, 1000);
+	};
+	updateClock();
+});
