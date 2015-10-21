3eb50c22bf4bf213b5128f81a2cd3b1e2c101deb
jelly_fly <jelly329@126.com>
2015-10-20 22:28:00 -0400

    parser

---
+++ book/14/app.js
@@ -0,0 +1,21 @@
+angular.module('myApp').
+directive('oneToTen', function() {
+	return {
+		require: '?ngModel',
+		link: function(scope, ele, attrs, ngModel) {
+			if (!ngModel) return;
+			ngModel.$parsers.unshift(
+				function(viewValue) {
+					var i = parseInt(viewValue);
+					if (i >= 0 && i < 10) {
+						ngModel.$setValidity('oneToTen', true);
+						return viewValue;
+					} else {
+						ngModel.$setValidity('oneToTen', false);
+						return undefined;
+					}
+				}
+			)
+		}
+	}
+});
